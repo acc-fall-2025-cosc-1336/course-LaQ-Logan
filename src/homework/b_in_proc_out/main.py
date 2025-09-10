9f75752e6339e9a1bb0f837fb5c0c2a122f0152d
LaQuira Logan <laquira.logan@g.austincc.edu>
2025-09-10 03:47:36 +0000

    HW Multiplying Numbers 5 and 7  by themselves

--- src/homework/b_in_proc_out/main.py
+++ src/homework/b_in_proc_out/main.py
@@ -1,1 +1,8 @@
-#
+from output import multiply_numbers
+
+def main():
+    result = multiply_numbers(5, 5), multiply_numbers(7, 7)
+    print("The result is:", result)
+
+if __name__ == "__main__":
+    main()
