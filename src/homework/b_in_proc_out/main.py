4118e0b6397fd87afcf6ef90de1ccd109a071797
LaQuira Logan <laquira.logan@g.austincc.edu>
2025-09-19 23:40:17 +0000

    Getting a Receipt with meal amount, sales tax, and tip

--- src/homework/b_in_proc_out/main.py
+++ src/homework/b_in_proc_out/main.py
@@ -2,7 +2,7 @@ from output import multiply_numbers
 
 def main():
     result = multiply_numbers(5, 5), multiply_numbers(7, 7)
-    print("The result is:", result)
+    print("The product's are: ", result)
 
 if __name__ == "__main__":
     main()
