fd9e8670608d97b524e4a44a1cfd181da842ca88
LaQuira Logan <laquira.logan@g.austincc.edu>
2025-10-02 21:42:14 +0000

    factorial and odd number summation

--- src/homework/d_repetition/main.py
+++ src/homework/d_repetition/main.py
@@ -1,7 +1,7 @@
 from repetition import get_factorial, sum_odd_numbers, run_menu, display_menu, handle_choice
 
 def main():
-    run_menu()
+    run_menu() # Start the menu, gets user input and handles choices
 
 if __name__ == "__main__":
     main()
